502f8a6db15fa3936a6179d7d96738fa01c1e893
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-04 10:33:31 -0800

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 158990680
+  local user = 114022194
 
   if matches[1] == "support" then
     user = 'user#id'..user
@@ -16,7 +16,7 @@ local function run(msg, matches)
   if msg.to.type == 'chat' then
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
-    return "سازنده ربات وارد گروه شد"
+    return "my owner is comimg"
   else 
     return 'This isnt a chat group!'
   end
@@ -24,15 +24,14 @@ local function run(msg, matches)
 end
 
 return {
-  description = "support", 
+  description = "father", 
   patterns = {
-    "^[!/](support)$"
+    "^[!/](father)$"
   }, 
   run = run 
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright; @vvViRuSss
+
+--ch : @zvirusx
