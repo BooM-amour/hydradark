5ee009e5e9dd51e3f3ac1a9c71983c8bff93e617
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-04 10:18:29 -0800

    Update download_media.lua

--- plugins/download_media.lua
+++ plugins/download_media.lua
@@ -31,7 +31,6 @@ return {
 	},
   pre_process = pre_process
 }
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright and edit; @vvViRuSss
+
+--ch : @zvirusx
